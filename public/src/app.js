0d0abf2502ea945fae8e10a10c85f9accac720e2
Mladen Nedev <mlados43@gmail.com>
2022-05-24 18:26:53 +0300

    fixed some things

---
+++ public/src/app.js
@@ -0,0 +1,23 @@
+// // Basic Firebase SDK Code (Temporary)!!!
+
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//     apiKey: "AIzaSyAPqoJAfhwCrLK6I-89pWdWZBKhiWTej_Q",
+//     authDomain: "food-store-website-53380.firebaseapp.com",
+//     projectId: "food-store-website-53380",
+//     storageBucket: "food-store-website-53380.appspot.com",
+//     messagingSenderId: "278973614683",
+//     appId: "1:278973614683:web:f06f820e09994ab3ec4d7e",
+//     measurementId: "G-B37HSCNN66"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
